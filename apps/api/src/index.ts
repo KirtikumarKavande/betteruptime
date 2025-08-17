66b9d14b8e077d2d0550c7e3edf60c41ac4a6276
Kirtikumar Kavande <121145138+KirtikumarKavande@users.noreply.github.com>
2025-08-17 07:43:44 +0000

    feat(api): set up TypeScript configuration and update API endpoints -made connection with database -added sample data in database in order to insure proper connection has been established

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,25 @@
-import { prismaClient } from "@repo/db/client";
+import {prismaClient}  from "@repo/db/client";
 import express from "express";
-const app=express()
-prismaClient.website.create({
-    data:{
-        url: "https://example.com",
-    }
+
+const app = express();
+app.use(express.json());
+
+app.post('/register',async(req,res)=>{
+const {url} = req.body;
+await prismaClient.website.create({
+            data: {
+                url: url,
+            }
+        });
+
+        res.send("post is working")
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+app.get('/health',(req,res)=>{
+    res.send("ok")
+})
+
+app.listen(3001,()=>{
+    console.log("server is running")
+})
 
